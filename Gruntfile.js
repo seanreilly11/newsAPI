3b1961e24480ad00630ae35b512c76afaf4bfaac
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-19 02:17:35 +0000

    functionality complete

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,65 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      all: {
+        files: ['js/script.js', 'css/style.css', 'index.html'],
+        tasks: ['csslint','jshint','validation']
+      }
+    },
     jshint: {
-    all: ['Gruntfile.js', 'js/*.js']
-  }
+      all: ['Gruntfile.js', 'js/*.js']
+    },
+    validation: {
+      // options: {
+      //   reset: grunt.option('reset') || false,
+      //   stoponerror: false,
+      //   remotePath: 'http://decodize.com/',
+      //   remoteFiles: ['html/moving-from-wordpress-to-octopress/',
+      //                 'css/site-preloading-methods/'], //or
+      //   remoteFiles: 'validation-files.json', // JSON file contains array of page paths.
+      //   relaxerror: ['Bad value X-UA-Compatible for attribute http-equiv on element meta.'], //ignores these errors
+      // generateReport: false
+      //   errorHTMLRootDir: "w3cErrorFolder",
+      //   useTimeStamp: true,
+      //   errorTemplate: "w3c_validation_error_Template.html"
+      // },
+      files: {
+        src: ['index.html']
+      }
+    },
+    csslint: {
+      // strict: {
+      //   options: {
+      //     import: 2
+      //   },
+      //   src: ['path/to/**/*.css']
+      // },
+      lax: {
+        options: {
+          import: false
+        },
+        src: ['css/*.css']
+      }
+    },
+    uglify: {
+      build: {
+        src: "js/script.js",
+        dest: "js/script.min.js"
+      }
+    }
   });
 
   // grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-w3c-html-validation');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['validation','jshint', 'csslint']);
+  grunt.registerTask('ugly', ['uglify']);
 
 };
--- js/script.js
+++ js/script.js
@@ -1,33 +1,16 @@
 $(document).ready(function(){
-	var country, category, source;
+	var country, category, source, search;
 
 	defaultDisplay();
-
-	function defaultDisplay(){
-		$.ajax({
-			url: 'http://newsapi.org/v2/top-headlines?country=nz&apiKey=f86d2f8d630843db90156d4555dab9cb',
-			type: 'GET',
-			data: 'json',
-			success: function(data){
-				document.getElementById("output").innerHTML = "";
-				for(var i = 0; i < data.articles.length; i++){
-					if(data.articles[i].urlToImage == null) data.articles[i].urlToImage = 'images/News.jpg';
-					document.getElementById("output").innerHTML += '<div class="card col-4"><img src="'+data.articles[i].urlToImage+'" class="card-img-top">'
-					+ '<div class="card-body"><h5 class="card-title">' + data.articles[i].title +'</h5>'
-					+ '<p class="card-text">' + data.articles[i].description + '</p>'
-					+ '<a class="card-text" href="' + data.articles[i].url +'">View story here</a></p>'
-					+ '</div></div>';
-				}
-			},
-			error: function(){
-				console.log("error");
-			}
-	}); //end of ajax
-	}
 
 	// disable source if either select is used
 	if(($("#country").val() != "0") || ($("#category").val() != "0")){
 		$("#source").attr("disabled", true);
+	}
+
+	if($("#source").val() != "0"){
+		$("#country").attr("disabled", true);
+		$("#category").attr("disabled", true);
 	}
 	
 	$("#country").change(function(){
@@ -47,29 +30,45 @@ $(document).ready(function(){
 			$("#source").attr("disabled", true);
 		}
 	});
+
+	$("#source").change(function(){
+		if($("#source").val() === "0"){
+			$("#country").attr("disabled", false);
+			$("#category").attr("disabled", false);
+		}
+		if($("#source").val() != "0"){
+			$("#country").attr("disabled", true);
+			$("#category").attr("disabled", true);
+		}
+	});
 	
 
 	$("#submit").click(function(){
 		country = document.getElementById("country").value;
 		category = document.getElementById("category").value;
 		source = document.getElementById("source").value;
+		search = document.getElementById("search-term").value;
 		buildURL();
-	})
+	});
 
 	$("#reset").click(function(){
 		$('#country').prop('selectedIndex',0);
 		$('#category').prop('selectedIndex',0);
 		$('#source').prop('selectedIndex',0);
 		$("#source").attr("disabled", false);
+		$("#category").attr("disabled", false);
+		$("#country").attr("disabled", false);
+		document.getElementById("search-term").value = "";
 		defaultDisplay();
-	})
+	});
 
 	function buildURL(){
 		var baseURL = 'http://newsapi.org/v2/top-headlines?';
 
-		if(country != 0)baseURL += `country=${country}&`;
-		if(category != 0)baseURL += `category=${category}&`;
-		if(source != 0)baseURL += `sources=${source}&`;
+		if(country != 0)baseURL += "country=" + country + "&";
+		if(category != 0)baseURL += "category=" + category+ "&";
+		if(source != 0)baseURL += "sources=" +source +"&";
+		if(search != "")baseURL += "q=" +search +"&";
 		if (baseURL ===  'http://newsapi.org/v2/top-headlines?')baseURL += "country=nz&";
 		baseURL += "apiKey=f86d2f8d630843db90156d4555dab9cb";
 
@@ -85,18 +84,47 @@ $(document).ready(function(){
 				console.log(data.articles);
 				document.getElementById("output").innerHTML = "";
 				for(var i = 0; i < data.articles.length; i++){
+					var card = "";
 					if(data.articles[i].urlToImage == null) data.articles[i].urlToImage = 'images/News.jpg';
-					document.getElementById("output").innerHTML += '<div class="card col-4"><img src="'+data.articles[i].urlToImage+'" class="card-img-top">'
-					+ '<div class="card-body"><h5 class="card-title">' + data.articles[i].title +'</h5>'
-					+ '<p class="card-text">' + data.articles[i].description + '</p>'
-					+ '<a class="card-text" target="_blank" href="' + data.articles[i].url +'">View story here</a></p>'
+					card += '<div class="card col-4"><img src="'+data.articles[i].urlToImage+'" class="card-img-top">'
+					+ '<div class="card-body"><h5 class="card-title">' + data.articles[i].title +'</h5>';
+					if(data.articles[i].description != null){card += '<p class="card-text">' + data.articles[i].description + '</p>';}
+					card += '<a class="card-text" target="_blank" href="' + data.articles[i].url +'">View story here</a></p>'
 					+ '</div></div>';
+
+					document.getElementById("output").innerHTML += card
+				}
+
+			},
+			error: function(){
+				console.log("error");
+			}
+		}); //end of ajax
+	}
+
+	function defaultDisplay(){
+		$.ajax({
+			url: 'http://newsapi.org/v2/top-headlines?country=nz&apiKey=f86d2f8d630843db90156d4555dab9cb',
+			type: 'GET',
+			data: 'json',
+			success: function(data){
+				document.getElementById("output").innerHTML = "";
+				for(var i = 0; i < data.articles.length; i++){
+					var card = "";
+					if(data.articles[i].urlToImage == null) data.articles[i].urlToImage = 'images/News.jpg';
+					card += '<div class="card col-4"><img src="'+data.articles[i].urlToImage+'" class="card-img-top">'
+					+ '<div class="card-body"><h5 class="card-title">' + data.articles[i].title +'</h5>';
+					if(data.articles[i].description != null){card += '<p class="card-text">' + data.articles[i].description + '</p>';}
+					card += '<a class="card-text" target="_blank" href="' + data.articles[i].url +'">View story here</a></p>'
+					+ '</div></div>';
+
+					document.getElementById("output").innerHTML += card
 				}
 			},
 			error: function(){
 				console.log("error");
 			}
-	}); //end of ajax
+		}); //end of ajax
 	}
 });
 
