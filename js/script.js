56a45f83357be182b3d1534ff134b1510fd85ad2
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-20 02:03:19 +0000

    completed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 $(document).ready(function(){
 	var country, category, source, search;
-
-	defaultDisplay();
-
+	//print default
+	buildURL();
 	// disable source if either select is used
 	if(($("#country").val() != "0") || ($("#category").val() != "0")){
 		$("#source").attr("disabled", true);
 	}
-
 	if($("#source").val() != "0"){
 		$("#country").attr("disabled", true);
 		$("#category").attr("disabled", true);
 	}
-	
+	// disable source on change
 	$("#country").change(function(){
 		if($("#country").val() === "0"){
 			$("#source").attr("disabled", false);
@@ -21,7 +19,6 @@ $(document).ready(function(){
 			$("#source").attr("disabled", true);
 		}
 	});
-	
 	$("#category").change(function(){
 		if($("#category").val() === "0"){
 			$("#source").attr("disabled", false);
@@ -30,7 +27,7 @@ $(document).ready(function(){
 			$("#source").attr("disabled", true);
 		}
 	});
-
+	// disable when source is selected
 	$("#source").change(function(){
 		if($("#source").val() === "0"){
 			$("#country").attr("disabled", false);
@@ -41,8 +38,7 @@ $(document).ready(function(){
 			$("#category").attr("disabled", true);
 		}
 	});
-	
-
+	// show results
 	$("#submit").click(function(){
 		country = document.getElementById("country").value;
 		category = document.getElementById("category").value;
@@ -50,38 +46,31 @@ $(document).ready(function(){
 		search = document.getElementById("search-term").value;
 		buildURL();
 	});
-
+	// reset back to default display and reset options
 	$("#reset").click(function(){
-		$('#country').prop('selectedIndex',0);
-		$('#category').prop('selectedIndex',0);
-		$('#source').prop('selectedIndex',0);
-		$("#source").attr("disabled", false);
-		$("#category").attr("disabled", false);
-		$("#country").attr("disabled", false);
+		$('.custom-select').prop('selectedIndex',0);
+		$(".custom-select").attr("disabled", false);
 		document.getElementById("search-term").value = "";
-		defaultDisplay();
+		print("http://newsapi.org/v2/top-headlines?country=nz&apiKey=f86d2f8d630843db90156d4555dab9cb");
 	});
-
+	// create url based on what options are actually chosen
 	function buildURL(){
 		var baseURL = 'http://newsapi.org/v2/top-headlines?';
-
-		if(country != 0)baseURL += "country=" + country + "&";
-		if(category != 0)baseURL += "category=" + category+ "&";
-		if(source != 0)baseURL += "sources=" +source +"&";
-		if(search != "")baseURL += "q=" +search +"&";
+		if(country != 0 && country != undefined)baseURL += "country=" + country + "&";
+		if(category != 0 && category != undefined)baseURL += "category=" + category+ "&";
+		if(source != 0 && source != undefined)baseURL += "sources=" +source +"&";
+		if(search != "" && search != undefined)baseURL += "q=" +search +"&";
 		if (baseURL ===  'http://newsapi.org/v2/top-headlines?')baseURL += "country=nz&";
 		baseURL += "apiKey=f86d2f8d630843db90156d4555dab9cb";
-
 		print(baseURL);
-	}
-
+	};
+	// print function to display 
 	function print(url){
 		$.ajax({
 			url: url,
 			type: 'GET',
 			data: 'json',
 			success: function(data){
-				console.log(data.articles);
 				document.getElementById("output").innerHTML = "";
 				for(var i = 0; i < data.articles.length; i++){
 					var card = "";
@@ -91,40 +80,10 @@ $(document).ready(function(){
 					if(data.articles[i].description != null){card += '<p class="card-text">' + data.articles[i].description + '</p>';}
 					card += '<a class="card-text" target="_blank" href="' + data.articles[i].url +'">View story here</a></p>'
 					+ '</div></div>';
-
-					document.getElementById("output").innerHTML += card
-				}
-
-			},
-			error: function(){
-				console.log("error");
-			}
-		}); //end of ajax
-	}
-
-	function defaultDisplay(){
-		$.ajax({
-			url: 'http://newsapi.org/v2/top-headlines?country=nz&apiKey=f86d2f8d630843db90156d4555dab9cb',
-			type: 'GET',
-			data: 'json',
-			success: function(data){
-				document.getElementById("output").innerHTML = "";
-				for(var i = 0; i < data.articles.length; i++){
-					var card = "";
-					if(data.articles[i].urlToImage == null) data.articles[i].urlToImage = 'images/News.jpg';
-					card += '<div class="card col-4"><img src="'+data.articles[i].urlToImage+'" class="card-img-top">'
-					+ '<div class="card-body"><h5 class="card-title">' + data.articles[i].title +'</h5>';
-					if(data.articles[i].description != null){card += '<p class="card-text">' + data.articles[i].description + '</p>';}
-					card += '<a class="card-text" target="_blank" href="' + data.articles[i].url +'">View story here</a></p>'
-					+ '</div></div>';
-
-					document.getElementById("output").innerHTML += card
+					document.getElementById("output").innerHTML += card;
 				}
 			},
-			error: function(){
-				console.log("error");
-			}
+			error: function(){console.log("error");}
 		}); //end of ajax
 	}
 });
-
